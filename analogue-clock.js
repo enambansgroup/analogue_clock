8d97983306da5bd22cf9bee080a95f009841ec9a
Bansah Mawuenam <103264447+enambansgroup@users.noreply.github.com>
2024-06-16 06:45:13 +0000

    updated clock appearance
    clock is functional now. 
    thinking of making it more sophisticated so will be making more update

--- analogue-clock.js
+++ analogue-clock.js
@@ -2,26 +2,22 @@ let sc = document.getElementById("sc");
 let mn = document.getElementById("mn");
 let hr = document.querySelector("#hr")
 
-setInterval (() =>{
-let day = new Date();
-let hh = day.getHours();
-let mm = day.getMinutes();
-let ss = day.getSeconds();
 
-let hH = 30 * hh + mm/2;
-let mH = 6 * mm;
-let sH = 6 * ss;
+ updateTime = () =>{
+let day = new Date(),
+ hh = day.getHours();
+ mm = day.getMinutes();
+ ss = day.getSeconds();
 
-hr.style.transform = "rotateZ(${hH}deg)";
-mn.style.transform = "rotateZ(${mH}deg)";
-sc.style.transform = "rotateZ(${sH}deg)";
-}, 1000);
+let hH = (hh / 12) * 360;
+let mH = (mm / 60) * 360;
+let sH = (ss / 60) * 360;
 
+hr.style.transform = `rotate(${hH}deg)`;
+mn.style.transform = `rotate(${mH}deg)`;
+sc.style.transform = `rotate(${sH}deg)`;
+};
 
-// let b = document.querySelector("body")
-// b.style.background = "#"
-
-hr.style.backgroundColor = "red";
-hr.style.transform = rotate (180deg)
-
+setInterval(updateTime, 1000);
+updateTime();
 
